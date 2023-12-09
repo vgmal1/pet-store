46a5aae93f1b0d9e4c8e49aed4c869f50fffee4d
vgmal1 <125531701+vgmal1@users.noreply.github.com>
2023-12-09 14:15:37 -0700

    second commit

---
+++ src/main/java/pet/store/entity/Employee.java
@@ -0,0 +1,26 @@
+package pet.store.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long employeeId;
+	private String employeeFirstName;
+	private String employeeLastName;
+	private String employeePhone;
+	private String employeeJobTitle;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "pet_store_id")
+	private PetStore petStore = new PetStore();
+}
